424ebe3d3e242b14a341c0a61005a64c675b2669
itz_abdulfatai <134066346+itz-abdulfatai@users.noreply.github.com>
2024-11-24 15:08:00 +0100

    edit sizes

--- frontend/src/assets/constants.js
+++ frontend/src/assets/constants.js
@@ -298,9 +298,9 @@ export const socials = [
 
 export const works = [
   {
-    name: 'travel      agency',
+    name: 'travel agency',
     img: null,
-    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum illum delectus, omnis veritatis, quam excepturi tempore perferendis maiores odit, quod sit. Eaque ut ullam cumque expedita corrupti illo! Vitae quia, voluptates enim illo amet similique, blanditiis, tenetur molestias unde quae et cumque nemo maiores ducimus dicta voluptatibus ad dolorum cupiditate.',
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum illum delectus, omnis veritatis, quam excepturi tempore perferendis maiores odit, quod sit. Eaque ut ullam cumque expcta voluptatibus ad dolorum cupiditate.',
     demo: ''
   },
   {
--- frontend/src/components/home/Works.jsx
+++ frontend/src/components/home/Works.jsx
@@ -24,7 +24,7 @@ function Works() {
         > <h3 className="relative capitalize font-extrabold hidden md:block text-center text-sm transition-all duration-200 ease-linear group-hover:-translate-y-20 ">{work.name}</h3>
           <div className="  absolute bottom-10 right-10 hidden md:flex text-2xl rounded-full aspect-square box-border  justify-center items-center border-2 p-2 group-hover:hidden"><i className='bx bx-pointer'></i></div>
             <div className=" h-full w-full relative md:-bottom-[500px]  md:group-hover:bottom-5 bg-[rgba(0,0,0,0.2)] rounded-3xl p-5 transition-all duration-200 ease-linear  flex flex-col gap-1 md:gap-5 items-center text-center justify-center glass-work backdrop-blur-xl border border-[#ffffff3b] group">
-                <h3 className=" capitalize font-[800]">{work.name}</h3>
+                <h3 className=" capitalize font-[800] text-sm">{work.name}</h3>
 
                 <p className=" text-xs md:text-sm" >{work.description}</p>
                 {/* <i className=' text-5xl bx bxs-right-top-arrow-circle  group-hover:text-secondary '></i> */}
